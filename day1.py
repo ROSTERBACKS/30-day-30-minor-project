9f97820687f0a99236c345cd53d04ac5fa594661
Roster backs <rushimire03@gmail.com>
2026-09-19 10:29:21 +0530

    Add Day 1 Python program

---
+++ day1.py
@@ -0,0 +1,7 @@
+#first minor ptoject
+name =input("Enter your name : ")
+age = int(input("Enter yiur age: "))
+print(f"Hey {name} .")
+print(f" Your current age was {age}.")
+print(f"next year will be your age was {age + 1}")
+
